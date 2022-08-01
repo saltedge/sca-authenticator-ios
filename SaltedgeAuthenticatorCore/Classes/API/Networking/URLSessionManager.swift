70d0a309403d82db1eb57d8254ab71d9863fc398
Daniel Marcenco <danmarc93@gmail.com>
2022-08-01 10:13:56 +0300

    Changed timeout interval to 20 seconds

--- SaltedgeAuthenticatorCore/Classes/API/Networking/URLSessionManager.swift
+++ SaltedgeAuthenticatorCore/Classes/API/Networking/URLSessionManager.swift
@@ -38,7 +38,7 @@ struct URLSessionManager {
 
     static func createSession() -> URLSession {
         let config: URLSessionConfiguration = .default
-        config.timeoutIntervalForRequest = 10.0
+        config.timeoutIntervalForRequest = 20.0
         config.timeoutIntervalForResource = 30.0
         config.requestCachePolicy = .reloadIgnoringLocalCacheData
 
